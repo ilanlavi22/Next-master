244b57683961062c5898e39d7eea8095f69df6bf
Ilan Lavi <refaelberlin@gmail.com>
2025-03-12 12:20:39 +0100

    sloving not found vercel

--- next.config.ts
+++ next.config.ts
@@ -17,9 +17,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  reactComponentAnnotation: {
-    enabled: process.env.NODE_ENV === "development",
-  },
 };
 
 export default nextConfig;
